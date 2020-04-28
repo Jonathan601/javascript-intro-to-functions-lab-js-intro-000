9a3b02f64195f0398aba0fe97214701ea7dbdf9d
Jonathan Olivencia <jonathan@trainwithjon.com>
2020-04-28 03:10:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(hello){
+  return hello.toUpperCase()
+}
+function whisper(HELLO){
+  return HELLO.toLowerCase()
+}
+function logShout(hello){
+  console.log(hello.toUpperCase())
+}
+function logWhisper(HELLO){
+  console.log(HELLO.toLowerCase())
+}
